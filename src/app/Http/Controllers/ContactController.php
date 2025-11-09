78e69857a739ebc790eb1ceb6707fdf666139506
keita kato <infotec62509@gmail.com>
2025-11-09 14:25:26 +0900

    お問い合わせ内容確認ページの作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
 use App\Models\Category;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -16,7 +17,23 @@ class ContactController extends Controller
 
     public function confirm(ContactRequest $request)
     {
-        $categories = Category::all();
-        return view('confirm', compact('request', 'categories'));
+        $contact = $request->all();
+        $category = Category::find($contact['category_id']);
+        return view('confirm', compact('contact', 'category'));
+    }
+
+    public function confirm_test()
+    {
+        $contact = Contact::find(1);
+        $category = Category::find($contact->category_id);
+        return view('confirm', compact('contact', 'category'));
+    }
+
+    public function thanks()
+    {
+        $contact = $request->all();
+        $tel = $contact['tel1'] . $contact['tel2'] . $contact['tel3'];
+        unset($contact['tel1'], $contact['tel2'], $contact['tel3']);
+        $contact += array('tel' => $tel);
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,4 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::get('/confirm', [ContactController::class, 'confirm_test']);
